afe6639d4523577e4ee84edcf1d09829645dbf5a
Paul Cox <paulcox24@gmail.com>
2015-02-26 10:32:29 -0700

    Cleanup

--- oopcalc.rb
+++ oopcalc.rb
@@ -1,29 +1,29 @@
 class Calculator
   @@calc_count = 0
-  attr_accessor :value_1
+  attr_accessor :value1
   def initialize(value1,value2)
-    @value_1 = value1
-    @value_2 = value2
+    @value1 = value1
+    @value2 = value2
     @@calc_count += 1
-    puts "Your values are \n#{@value_1} and #{@value_2}"
+    puts "Your values are \n#{@value1} and #{@value2}"
   end
 
   # #getter methods
-  # def value_1
-  #     @value_1
+  # def value1
+  #     @value1
   # end
 
-  def value_2
-    @value_2
+  def value2
+    @value2
   end
 
   # # setter methods
-  # def value_1=(value)
-  #   @value_1 = value
+  # def value1=(value)
+  #   @value1 = value
   # end
 
-  def value_2=(value)
-    @value_2 = value
+  def value2=(value)
+    @value2 = value
   end
 
   def self.calc_count
@@ -32,32 +32,32 @@ class Calculator
 
 
   def add
-    result = @value_1 + @value_2
+    result = @value1 + @value2
     puts result
   end
 
   def subtract
-    result = @value_1 - @value_2
+    result = @value1 - @value2
     puts result
   end
 
   def multiply
-    result = @value_1 * @value_2
+    result = @value1 * @value2
     puts result
   end
 
   def divide
-    result = @value_1.to_f / @value_2.to_f
+    result = @value1.to_f / @value2.to_f
     puts result
   end         
 end
 
 puts "Welcome to Calculator please enter your first value"
 
-value_1 = gets.chomp.to_i
+value1 = gets.chomp.to_i
 
 puts "And your second"
 
-value_2 = gets.chomp.to_i
+value2 = gets.chomp.to_i
 
-@calc = Calculator.new(value_1,value_2)
+@calc = Calculator.new(value1,value2)
