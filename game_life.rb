beca94dd43cf0da931ac37fd6ad5505a18617cbe
Paul Cox <paulcox24@gmail.com>
2015-03-02 14:27:31 -0700

    Updating game of life

--- game_life.rb
+++ game_life.rb
@@ -1,8 +1,9 @@
-
-
 @board_size = 10
 @counter = 0 # initiate counter
 @times_to_run = 200
+@alive = '[]'
+@dead = '  '
+@percent_alive = 20
 
 # 	@board = 
 # [['x','o','x','o','x'],
@@ -41,11 +42,15 @@ end
 
 
 def random_board
-	a = ['[]','[]','  ','  ','  ','  ','  ','  ','  ','  ']*@board_size
+	# a = [@alive,@alive,'  ','  ','  ','  ','  ','  ','  ','  ']*@board_size
+	# alive_sample = Array.new(2 * @board_size) { |i| @alive }
+	# dead_sample = Array.new(8 * @board_size) { |i| @dead }
+	# sample_array = alive_sample + dead_sample
+	sample_array = Array.new(10 * @board_size) { |i| rand(0..100) <= @percent_alive ? @alive : @dead }
 	@board = []
-	@board_size.times { @board << a.sample(@board_size) }
+	@board_size.times { @board << sample_array.sample(@board_size) }
   @tracker = Array.new(@board_size) {Array.new(@board_size, 0)}
-  @living = Array.new(@board_size) {Array.new(@board_size, 0)}
+  # @living = Array.new(@board_size) {Array.new(@board_size, 0)}
 end
 
 
@@ -64,39 +69,31 @@ end
 
 #live
 def live(xpos, ypos)
-	@tracker[xpos][ypos] = '[]'	
+	@tracker[xpos][ypos] = @alive	
 end
 
 #kill
 def kill(xpos, ypos)
-	@tracker[xpos][ypos] = '  '	
+	@tracker[xpos][ypos] = @dead
 end
 
-def check_edge(xpos, ypos, xmove, ymove)
-	  x = xpos + xmove
+def check_edge(xpos, ypos, xmove, ymove) # check and adjacent cell for alive or dead
+  if xpos + xmove >= @board_size # return to index 0 if the modifier goes above the board
+    x = 0
+  elsif xpos + xmove < 0  # return to the end of the board if the 
+  	x = @board_size - 1
+  else
+  	x = xpos + xmove # new xposition from the modifyer
+  end
 
-	  if xpos + xmove >= @board_size
-      x = 0
-	  end
-
-	  if xpos + xmove < 0
-	  	x = @board_size - 1
-	  end
-
-	  y = ypos + ymove
-
-	  if ypos + ymove >= @board_size
-      y = 0
-	  end
-
-	  if ypos + ymove < 0
-	  	y = @board_size -1
-	  end
-
-	  if @board[x][y] == '[]'
-	   @surrounding_live += 1
-
-	  end
+  if ypos + ymove >= @board_size
+    y = 0
+  elsif ypos + ymove < 0
+  	y = @board_size -1
+  else
+  	y = ypos + ymove
+  end
+  @surrounding_live += 1 if @board[x][y] == @alive
 end
 
 
@@ -105,6 +102,7 @@ end
 #xpos is the row position, the index in the overall board array
 #ypos is the column position, the index in the row array in the board array
 def live_die #update tracker
+	@cells_alive = 0
 	@board.each_with_index do |row, xpos|
 		row.each_with_index do |cell, ypos|
 			@surrounding_live = 0
@@ -119,12 +117,13 @@ def live_die #update tracker
 
       #@living[xpos][ypos] = @surrounding_live
 
-			if cell == '[]'
+			if cell == @alive
 	      if @surrounding_live == 2 || @surrounding_live == 3
 	        live(xpos,ypos)
 	      else
 	        kill(xpos,ypos)
 	      end
+	      @cells_alive += 1
 	    else
 	      if @surrounding_live == 3
 		      live(xpos,ypos)
@@ -146,14 +145,15 @@ times_to_run
 random_board
 
 while @counter < @times_to_run
-	
 	system("clear")
-  #display_living
+  # display_living
 	display_board
 	@counter +=1
 	puts "Counter: #{@counter}"
+	puts "Cells Alive: #{@cells_alive}"
 	live_die
 	sleep(0.15)
+	break if @cells_alive <= 0
 
 end
 
